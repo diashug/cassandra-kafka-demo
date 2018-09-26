b054a237f031120c3b38e3e04036c5967f873a24
Hugo Dias <hmdias@gmail.com>
2018-09-26 22:56:35 +0100

    Add project files.

---
+++ CassandraKafkaDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CassandraKafkaDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
